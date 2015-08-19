22577dbd387b9f6c4c23c6df5bab517653e44149
Eric Myhre <hash@exultant.us>
2015-08-19 14:31:04 -0700

    add build scripts and integration hookup.
    integration is produced as a separate command; we may add more commands later (e.g. transformers and fmt-like tools).

---
+++ go/cmd/integ/integ.go
@@ -0,0 +1,14 @@
+/*
+	Executable designed for plug in to the integration/validation suite.
+
+	Reads yamelot on stdin; emits json on stdout.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("{}\n") // nope
+}
