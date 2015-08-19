31bc417e190e90a4895a7c2e3c3e9a37215e2aed
Jason Michalski <armooo@dropbox.com>
2015-08-19 11:23:22 -0700

    Add the ygp cffi module to setup.py

--- python/setup.py
+++ python/setup.py
@@ -6,5 +6,8 @@ setup(
     packages=find_packages(exclude=['cffi_build']),
     setup_requires=['cffi>=1.0.0'],
     install_requires=['cffi>=1.0.0'],
-    cffi_modules=['cffi_build/yaml_build.py:ffi'],
+    cffi_modules=[
+        'cffi_build/yaml_build.py:ffi',
+        'cffi_build/ygp_build.py:ffi',
+    ],
 )
