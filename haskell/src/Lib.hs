5aa13249ce2cb87d21ab3053b4919d018e51feb5
Greg Price <gregprice@dropbox.com>
2015-08-20 01:09:50 -0700

    haskell: set up to annotate with indentation; generalize tests

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -1,12 +1,21 @@
 module Lib
     ( parseYamelot
     , Value(..)
+    , annotateIndents  -- urgh, exported for testing
     ) where
 
 import Text.ParserCombinators.Parsec
 
 data Value = Scalar String | Seq [Value]
   deriving (Eq, Show)
+
+
+annotateIndents :: String -> [(Int, Char)]
+annotateIndents = annotate 0
+  where annotate n ('\n':s) = (n,'\n') : annotate 0 s
+        annotate n (c:s)    = (n,c)    : annotate (n+1) s
+        annotate _ []       = []
+
 
 flowSequence = between (char '[') (char ']') $
                  Seq <$> sepEndBy flowNode (char ',')
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -4,21 +4,29 @@ import System.Exit
 
 import Lib
 
-check :: (String, Maybe Value) -> IO ()
-check (input, expected) = case parseYamelot input of
-      Right result | expected == Just result -> return ()
-      Left _       | expected == Nothing     -> return ()
-      outcome                                ->
-         do putStrLn $ "failed: " ++ input
+check :: (Show a, Show b, Eq b) => String -> (a -> b) -> (a, b) -> IO ()
+check msg f (input, expected) = case f input of
+      result | expected == result -> return ()
+      result                      ->
+         do putStrLn $ "failed " ++ msg ++ ": " ++ show input
             putStrLn $ "  expected: " ++ show expected
-            putStrLn $ "  got: " ++ show outcome
+            putStrLn $ "  got: " ++ show result
             exitFailure
 
+checkParse :: (String, Maybe Value) -> IO ()
+checkParse = check "parse" (massage . parseYamelot)
+  where massage (Right result) = Just result
+        massage (Left _)       = Nothing
+
 main :: IO ()
-main = mapM_ check [ ("[1,2a]", Just (Seq [Scalar "1", Scalar "2a"]))
-                   , ("[1]", Just (Seq [Scalar "1"]))
-                   , ("[]", Just (Seq []))
-                   , ("[1,[]]", Just (Seq [Scalar "1", Seq []]))
-                   , ("[,]", Nothing)
-                   , ("[1,]", Just (Seq [Scalar "1"]))
-                   ]
+main = do mapM_ checkParse [
+              ("[1,2a]", Just (Seq [Scalar "1", Scalar "2a"]))
+            , ("[1]", Just (Seq [Scalar "1"]))
+            , ("[]", Just (Seq []))
+            , ("[1,[]]", Just (Seq [Scalar "1", Seq []]))
+            , ("[,]", Nothing)
+            , ("[1,]", Just (Seq [Scalar "1"]))
+           ]
+          mapM_ (check "annotate" annotateIndents) [
+              ("ab\n cd", [(0,'a'),(1,'b'),(2,'\n'),(0,' '),(1,'c'),(2,'d')])
+           ]
