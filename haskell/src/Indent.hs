4b9c1db4e0654104645f95e729deafe0f45e2083
Greg Price <gregprice@dropbox.com>
2015-08-20 22:34:23 -0700

    haskell: Rip out Tree, have grammar specify values

--- haskell/src/Indent.hs
+++ haskell/src/Indent.hs
@@ -10,70 +10,74 @@ type Range = (Int, Int) -- inclusive
 inRange :: Int -> Range -> Bool
 inRange i (j, k) = i >= j && i <= k
 inf = 9999999
-type Parse = [(Token, Int)]
-          -> Range
-          -> Bool -- absolute alignment
-          -> Maybe (Tree, [(Token, Int)], Range, Bool)
+type Parse a = [(Token, Int)]
+            -> Range
+            -> Bool -- absolute alignment
+            -> Maybe (a, [(Token, Int)], Range, Bool)
 
-data Tree = Epsilon
-          | Terminal Char
-          | Lookahead
-          | Sequence Tree Tree
-          | ChoiceLeft Tree
-          | ChoiceRight Tree
-          | Star [Tree]
-    deriving (Show)
+{-
+instance Functor Parse where
+  fmap ff p cs i f = fmap (\(a, cs', i', f') -> (ff a, cs', i', f'))
+-}
 
-epsilon :: Parse
-epsilon cs i f = Just (Epsilon, cs, i, f)
+ffmap :: (a -> b) -> Parse a -> Parse b
+ffmap ff = fmap $ fmap $ fmap $ fmap
+             (\(a, cs', i', f') -> (ff a, cs', i', f'))
 
-term :: Char -> Parse
+data Value = Scalar Char
+           | Sequence [Value]
+  deriving (Show)
+
+epsilon :: Parse ()
+epsilon cs i f = Just ((), cs, i, f)
+
+term :: Char -> Parse Char
 term _ [] _ _ = Nothing
 term a ((c,k):cs) i f
-    | a == c && k `inRange` i = trace ("term " ++ show a ++ ": " ++ show (munge ((c,k):cs), k, i)) $ Just (Terminal c, cs, (k, k), False)
+    | a == c && k `inRange` i = trace ("term " ++ show a ++ ": " ++ show (munge ((c,k):cs), k, i)) $ Just (c, cs, (k, k), False)
     | otherwise = trace ("term " ++ show a ++ " failed: " ++ show (munge ((c,k):cs), k, i)) $ Nothing
 
 munge = map fst
 
-sq :: Parse -> Parse -> Parse
+sq :: Parse a -> Parse b -> Parse (a,b)
 sq p1 p2 cs i f =
     case p1 cs i f of
         Nothing -> Nothing
-        Just (t, cs', i', f') -> case p2 cs' i' f' of
+        Just (a, cs', i', f') -> case p2 cs' i' f' of
             Nothing -> Nothing
-            Just (t', cs'', i'', f'') -> Just (Sequence t t', cs'', i'', f'')
+            Just (b, cs'', i'', f'') -> Just ((a,b), cs'', i'', f'')
 
-lookahead :: Parse -> Parse
-lookahead p cs i f =
-    case p cs i f of
-        Nothing -> Nothing
-        Just (_, _, _, _) -> Just (Lookahead, cs, i, f)
+sql :: Parse a -> Parse b -> Parse a
+sql p1 p2 = ffmap fst (p1 `sq` p2)
 
-choice :: Parse -> Parse -> Parse
+sqr :: Parse a -> Parse b -> Parse b
+sqr p1 p2 = ffmap snd (p1 `sq` p2)
+
+choice :: Parse a -> Parse a -> Parse a
 choice p1 p2 cs i f =
     case p1 cs i f of
         Nothing -> case p2 cs i f of
             Nothing -> Nothing
-            Just (t, cs', i', f') -> Just (ChoiceRight t, cs', i', f')
-        Just (t, cs', i', f') -> Just (ChoiceLeft t, cs', i', f')
+            Just (a, cs', i', f') -> Just (a, cs', i', f')
+        Just (a, cs', i', f') -> Just (a, cs', i', f')
 
-star :: Parse -> Parse
+star :: Parse a -> Parse [a]
 star p cs i f =
     case p cs i f of
-        Nothing -> trace ("star empty: " ++ show (munge cs, i)) $ Just (Star [], cs, i, f)
-        Just (t, cs', i', f') ->
+        Nothing -> trace ("star empty: " ++ show (munge cs, i)) $ Just ([], cs, i, f)
+        Just (a, cs', i', f') ->
             case star p cs' i' f' of
                 Nothing -> Nothing
-                Just (Star ts, cs'', i'', f'') -> trace ("star: " ++ show (munge cs, munge cs'', i'')) $ Just (Star (t:ts), cs'', i'', f'')
+                Just (as, cs'', i'', f'') -> trace ("star: " ++ show (munge cs, munge cs'', i'')) $ Just (a:as, cs'', i'', f'')
 
-indent :: Indent -> Parse -> Parse
+indent :: Indent -> Parse a -> Parse a
 indent ind p cs i False =
     case p cs (fwd ind i) False of
         Nothing -> Nothing
-        Just (t, cs', i', f') -> Just (t, cs', i `intersect` bwd ind i', f')
+        Just (a, cs', i', f') -> Just (a, cs', i `intersect` bwd ind i', f')
 indent ind p cs i True = p cs i True
 
-fixFirst :: Parse -> Parse
+fixFirst :: Parse a -> Parse a
 fixFirst p cs i f = p cs i True
 
 data Indent = IGt | IGte | IEq | IAll
@@ -82,7 +86,7 @@ intersect (l,h) (l',h') = (max l l', min h h')
 
 plus p cs i f = case (p `sq` star p) cs i f of
     Nothing -> Nothing
-    Just (Sequence t (Star ts), cs', i', f') -> Just (Star (t:ts), cs', i', f')
+    Just ((t,ts), cs', i', f') -> Just (t:ts, cs', i', f')
 
 gt = indent IGt
 eq = indent IEq
@@ -107,25 +111,18 @@ tok xs = go startIx xs
           go i (' ':xs) = (' ', i) : go (i+1) xs
           go i (x:xs) = (x, i) : go (i+1) xs
 
-run :: String -> Parse -> Maybe (Tree, String)
+run :: String -> Parse a -> Maybe (a, String)
 run cs p = fmap (\(t,xs,_,_) -> (t,munge xs)) (p (tok cs) (0, inf) False)
-
-
-gram3 cs i f = trace ("gram3: " ++ show i) $ case
-        (term 'a' `sq` gt (star (fixFirst (indent IEq gram3))))
-        cs i f of
-    Nothing -> trace "gram3 failed" $ Nothing
-    r@(Just (t, cs', i', f')) -> trace ("gram3 exit: " ++ show r) $ r
 
 gterm = gt . term
 
 ws = star $ iall $ term ' ' `choice` term '\n'
 
-list = plus (eq item)
-item = eq (term '-') `sq` ws `sq` (other `choice` gt list) `sq` ws
-other = gterm 'a' `choice` flow_collection
+list = ffmap Sequence $ plus (eq item)
+item = eq (term '-') `sqr` ws `sqr` (other `choice` gt list) `sql` ws
+other = ffmap Scalar (gterm 'a') `choice` flow_collection
 flow_collection = flow_list
-flow_list = gterm '[' `sq` ws `sq` star (gte flow `sq` ws) `sq` gte (gterm ']')
-flow = term 'b' `choice` flow_collection
+flow_list = ffmap Sequence $ gterm '[' `sqr` ws `sqr` star (gte flow `sql` ws) `sql` gte (gterm ']')
+flow = ffmap Scalar (term 'b') `choice` flow_collection
 
 yamelot = list
