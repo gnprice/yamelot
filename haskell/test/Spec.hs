bae28bdeb3d50c8c33c8636852ba1a2f49ff3023
Greg Price <gregprice@dropbox.com>
2015-08-20 21:49:26 -0700

    haskell: A few more tests and a crude framework

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -4,8 +4,8 @@ import System.Exit
 
 import Indent
 
-check :: (Show a, Show b, Eq b) => String -> (a -> b) -> (a, b) -> IO ()
-check msg f (input, expected) = case f input of
+check :: (Show a, Show b, Eq b) => String -> (a -> b) -> b -> a -> IO ()
+check msg f expected input = case f input of
       result | expected == result -> return ()
       result                      ->
          do putStrLn $ "failed " ++ msg ++ ": " ++ show input
@@ -13,10 +13,15 @@ check msg f (input, expected) = case f input of
             putStrLn $ "  got: " ++ show result
             exitFailure
 
-checkParse :: String -> IO ()
-checkParse input = check "parse" (fmap snd . flip run yamelot) (input, Just "")
+checkParse :: Bool -> String -> IO ()
+checkParse True = check "parse" (fmap snd . flip run yamelot) (Just "")
+checkParse False = check "parse" (fmap ((>0) . length . snd) . flip run yamelot) (Just True)
+
 
 main :: IO ()
-main = do mapM_ checkParse [
-              ("- [b]")
+main = do mapM_ (uncurry checkParse) [
+              (True, "- [b]")
+            , (True, "- a")
+            , (False, "- [b]a")
+            , (True, "-\n -\n  -a")
             ]
