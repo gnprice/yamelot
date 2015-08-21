7ce86b257ce012b11faff19b61afc51b4f6ce80e
Josiah Boning <josiah@dropbox.com>
2015-08-20 23:46:39 -0700

    Wire haskell up to the test framework

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
+import Data.List (intercalate)
 import Indent
+import System.Exit
+
+escape :: Char -> String
+escape c = case c of
+    '"' -> "\\\""
+    '\\' -> "\\\\"
+    '\b' -> "\\b"
+    '\f' -> "\\f"
+    '\n' -> "\\n"
+    '\r' -> "\\r"
+    '\t' -> "\\t"
+    c' -> [c']
+
+dumpJson :: Value -> String
+dumpJson (Scalar s) = "\""++(concat $ map escape s)++"\""
+dumpJson (Sequence vs) = "["++(intercalate "," $ map dumpJson vs)++"]"
 
 main :: IO ()
-main = putStrLn $ show $ parseYamelot "[1,11,111]"
+main = do
+    input <- getContents
+    case run input yamelot of
+        Just (v, s) -> putStrLn $ dumpJson v
+        Nothing -> do
+            putStrLn "error!"
+            exitFailure
