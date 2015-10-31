4097a528ad4f1d5030dba1d477e60ab3fe4d8600
Greg Price <gregprice@dropbox.com>
2015-10-31 16:25:55 -0700

    New test format for many cases per file; implement for Haskell
    In the Haskell implementation we've been writing a bunch of small test
    cases into its own unit tests, because the integration test format
    we've been using is kind of cumbersome.  Make a new format where
     * we can have a bunch of small test cases in one file, so the overhead
       is lower for writing a bunch of them;
     * we can have comments;
     * we can test behavior on documents that lack a final newline;
     * we can mark some tests as expected to fail in some implementations.
    
    I'm not totally happy with the xfail markings, because they really
    belong more to an implementation than to the tests themselves, which
    are shared.  But this is the easiest place to put them at least for now.
    
    Should document the format, the better to implement elsewhere.
    
    Also in copying a bunch of literal-scalar-related tests from the
    Haskell unit tests to here, I find that we're wrong on a couple of
    them -- we parse them, as we should, but we get the wrong result.
    Good thing this format goes beyond "does it parse"!  The Haskell
    unit tests didn't, because that was going to be more of a pain.

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
 import Data.List (intercalate)
+import Data.List.Split
+import Data.Maybe
+import Data.String
+import qualified Regex.RE2 as RE
 import System.Environment
 import System.IO
 import System.Console.GetOpt
@@ -36,10 +43,52 @@ yamelotToJson = do
 
 -- Running a sequence of tests
 
-processTests :: IO ()
-processTests = do
-      hPutStrLn stderr "coming soon!"
-      exitFailure
+runTestCase :: String -> IO ()
+runTestCase caseStr =
+    case RE.find pattern case_ of
+      Nothing -> die ("parse error in test case: " ++ show case_)
+      Just m -> if expected == actual
+                then putStrLn "pass"
+                else if attrXfail
+                     then putStrLn "xfail"
+                     else printFailure document expected actual
+        where attrs = BC.split ' ' (fromMaybe "" (RE.matchGroup m 1))
+              input = fromMaybe "" (RE.matchGroup m 2)
+              expected = RE.matchGroup m 3
+              attrChomped = elem "chomped" attrs
+              attrXfail = elem "Xhaskell" attrs
+              document = if attrChomped
+                         then input
+                         else BC.append input "\n"
+              actual = case run (BC.unpack document) yamelot of
+                Just (v, []) -> Just (BC.pack (dumpJson v))
+                Just (v, _) -> Nothing
+                Nothing -> Nothing
+  where caseBytes = BC.pack caseStr
+        (case_, _) = RE.replaceAll "(?m)^###.*\n?" caseBytes ""
+        pattern :: RE.Pattern
+        pattern = fromString ("(?s)\\A" ++
+            "(?:ATTR ([^\n]*)\n)?" ++
+            "(.*)\n" ++
+            "(?:" ++
+                "!!!!!!!!!!\n" ++ "|" ++
+                "(?:----------\n(.*)\n)" ++
+            ")\\z")
+        printFailure document expected actual = do
+          putStrLn "fail!"
+          putStrLn ("  On input " ++ show document)
+          putStrLn ("  expected " ++ render expected)
+          putStrLn ("  but got " ++ render actual)
+          where render res = BC.unpack (fromMaybe "error" res)
+
+runTests :: IO ()
+runTests = do
+      input <- getContents
+      let cases = splitOn caseSeparator input
+      putStrLn (show (length cases) ++ " test cases")
+      mapM_ runTestCase cases
+      exitSuccess
+  where caseSeparator = replicate 40 '=' ++ "\n"
 
 -- The command line
 
@@ -66,6 +115,6 @@ main = do
     let (actions, positionals, errors) = getOpt RequireOrder options args
     opts <- foldl (>>=) (return startOptions) actions
     if optTest opts then
-      processTests
+      runTests
     else
       yamelotToJson
