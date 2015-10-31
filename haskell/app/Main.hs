2a5a59cf507e888d3e9ae7a244e5053ec08d3570
Greg Price <gregprice@dropbox.com>
2015-10-31 14:33:57 -0700

    haskell: Begin a CLI
    Thanks, anonymous author on Haskell wiki in 2006:
      https://wiki.haskell.org/High-level_option_handling_with_GetOpt
    
    This is a little awkward and I'm sure there's something fancier
    by now, but this is fine for a start.

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,8 +1,14 @@
 module Main where
 
 import Data.List (intercalate)
-import Indent
+import System.Environment
+import System.IO
+import System.Console.GetOpt
 import System.Exit
+
+import Indent
+
+-- Converting whole input to JSON
 
 escape :: Char -> String
 escape c = case c of
@@ -19,11 +25,47 @@ dumpJson :: Value -> String
 dumpJson (Scalar s) = "\""++(concat $ map escape s)++"\""
 dumpJson (Sequence vs) = "["++(intercalate "," $ map dumpJson vs)++"]"
 
+yamelotToJson :: IO ()
+yamelotToJson = do
+      input <- getContents
+      case run input yamelot of
+          Just (v, s) -> putStrLn $ dumpJson v
+          Nothing -> do
+              putStrLn "error!"
+              exitFailure
+
+-- Running a sequence of tests
+
+processTests :: IO ()
+processTests = do
+      hPutStrLn stderr "coming soon!"
+      exitFailure
+
+-- The command line
+
+data Options = Options { optTest :: Bool }
+
+startOptions :: Options
+startOptions = Options { optTest = False }
+
+options :: [OptDescr (Options -> IO Options)]
+options = [
+    Option "t" ["test"]
+      (NoArg (\opt -> return opt { optTest = True }))
+      "Treat the input as a set of test cases"
+  , Option "" ["help"]
+      (NoArg (\_ -> do name <- getProgName
+                       hPutStrLn stderr (usageInfo name options)
+                       exitSuccess))
+      "Show a usage message"
+  ]
+
 main :: IO ()
 main = do
-    input <- getContents
-    case run input yamelot of
-        Just (v, s) -> putStrLn $ dumpJson v
-        Nothing -> do
-            putStrLn "error!"
-            exitFailure
+    args <- getArgs
+    let (actions, positionals, errors) = getOpt RequireOrder options args
+    opts <- foldl (>>=) (return startOptions) actions
+    if optTest opts then
+      processTests
+    else
+      yamelotToJson
